4413762cd28c47aea910f726e0cccc40d636ba6d
Maysoon Tabali <mtab03@me.com>
2020-07-19 17:25:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def my_hash
   }
   # return a valid hash with any key/value pair of your choice
 end
-  my_hash 
+  
   
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
